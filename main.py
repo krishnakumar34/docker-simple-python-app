b3c7ee8ae2188ce13d66c57560a38ab056d42d99
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-09-25 09:23:42 +0530

    Update main.py
    Updated

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello():
   from pytube import YouTube
 url='https://www.youtube.com/watch?v=yWtWt-e_p24'
 yt=YouTube(url)
-return yt.streams.get_highest_resolution().url
+  return yt.streams.get_highest_resolution().url
 
 
 
