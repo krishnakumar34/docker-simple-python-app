547089f6776ed643f3faf1047a8d8f8b14a10c4f
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-09-25 08:38:50 +0530

    Update main.py
    Updated

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello Worldkk'
 
+@app.route('/test')
+def get_high_res(url):
+ from pytube import YouTube
+ yt = YouTube(url)
+
+ return yt.streams.get_highest_resolution().url
+
+
+url = 'https://www.youtube.com/watch?v=yWtWt-e_p24'
+
+m3u = get_high_res(url)
+print(m3u)
+return m3u
+
+
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
